9a9574c0a44dd8ecf2f13594fe92bafd3d3f494b
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-21 20:20:43 +0100

    adding \n to the string

--- push_error.c
+++ push_error.c
@@ -6,6 +6,6 @@
  */
 void push_error(stack_t *stack, unsigned int L_number)
 {
-	fprintf(stderr, "L%d: usage: push integer", L_number);
+	fprintf(stderr, "L%d: usage: push integer\n", L_number);
 	_exit_m(stack);
 }
